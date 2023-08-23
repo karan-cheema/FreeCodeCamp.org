2f2dc1a2475bcf646a00078dd1ef872a90944269
Karan Cheema <karancheema@protonmail.com>
2023-08-23 21:42:17 +0530

    Create Build JavaScript Objects.js

---
+++ JavaScript Algorithms and Data Structures/Basic Javascript/Build JavaScript Objects.js
@@ -0,0 +1,8 @@
+const myDog = {
+  // Only change code below this line
+  "name": "Pluto",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["humans", "food", "cats"]
+  // Only change code above this line
+};
