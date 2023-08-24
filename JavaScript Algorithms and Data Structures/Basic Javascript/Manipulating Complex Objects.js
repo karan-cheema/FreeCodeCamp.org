9ed4538a4a85980245478cb569ee2f4206b04ec5
Karan Cheema <karancheema@protonmail.com>
2023-08-24 21:35:43 +0530

    Create Manipulating Complex Objects.js

---
+++ JavaScript Algorithms and Data Structures/Basic Javascript/Manipulating Complex Objects.js
@@ -0,0 +1,26 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+
+  {
+    "artist": "Linkin Park",
+    "title": "Thousand Suns",
+    "release_year": 2014,
+    "formats": [
+      "CD",
+      "Spotify",
+      "Apple Music",
+      "YouTube"
+    ],
+  },
+
+];
