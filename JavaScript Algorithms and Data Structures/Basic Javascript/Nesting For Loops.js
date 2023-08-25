fe747a62dcb292a0f64d2818c418f8b9c91bc93b
Karan Cheema <karancheema@protonmail.com>
2023-08-25 21:57:19 +0530

    Create Nesting For Loops.js

---
+++ JavaScript Algorithms and Data Structures/Basic Javascript/Nesting For Loops.js
@@ -0,0 +1,13 @@
+function multiplyAll(arr) {
+  let product = 1;
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+      product = product * arr[i][j];
+    }
+  }
+  // Only change code above this line
+  return product;
+}
+
+// Modify values below to test your code
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
