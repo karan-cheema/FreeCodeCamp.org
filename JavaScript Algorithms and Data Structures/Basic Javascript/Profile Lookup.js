7bd94c42b67c9b4b1bb4410719cb79573ea5c04d
Karan Cheema <karancheema@protonmail.com>
2023-08-26 21:01:53 +0530

    Update Profile Lookup.js

--- JavaScript Algorithms and Data Structures/Basic Javascript/Profile Lookup.js
+++ JavaScript Algorithms and Data Structures/Basic Javascript/Profile Lookup.js
@@ -1,5 +1,34 @@
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
 function lookUpProfile(name, prop) {
-  for (let x = 0; x < contacts.length; x++) {
+  // Only change code below this line
+for (let x = 0; x < contacts.length; x++) {
     if (contacts[x].firstName === name) {
       if (contacts[x].hasOwnProperty(prop)) {
         return contacts[x][prop];
@@ -10,3 +39,6 @@ function lookUpProfile(name, prop) {
   }
   return "No such contact";
 }
+  // Only change code above this line
+
+lookUpProfile("Akira", "likes");
